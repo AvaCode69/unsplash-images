55a37ec64139f460c2bfcf35c6cc9d97d167def1
AvaCode69 <reacthmd34@gmail.com>
2023-05-20 01:03:53 +0200

    add react-query

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGlobalContext } from "./context";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const [title, setTitle] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+    console.log(title);
+  };
+
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          value={title}
+          placeholder="cat"
+        ></input>
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
