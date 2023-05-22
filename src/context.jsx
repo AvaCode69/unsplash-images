5520d272b7b2fd3be934b87cdea6b80144b364a9
AvaCode69 <reacthmd34@gmail.com>
2023-05-22 16:42:06 +0200

    add local storage

--- src/context.jsx
+++ src/context.jsx
@@ -2,13 +2,26 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "prefers-color-scheme:dark"
+  ).matches;
+  const storedDarkMode = localStorage.getItem("darkTheme") === "true";
+  return storedDarkMode || prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTheme, setSearchTheme] = useState("cat");
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
   };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
 
   return (
     <AppContext.Provider
